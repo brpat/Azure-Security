58fba55ebae48aaac3f04ba4a149089c31d1b616
Brijesh <brpatel0807@gmail.com>
2024-09-15 21:01:24 -0400

    Added initial activity logs client

--- modules/activity_logs.py
+++ modules/activity_logs.py
@@ -18,13 +18,12 @@ def return_activity_logs(resource_group):
     activity_logs = mmc_client.activity_logs.list(filter=filter)
     
     for log in activity_logs:
-        json.loads(str(log))
-        caller = log.get("caller", "None")
-        event = log.get("message", "None")
-        operation_name = log.get("operation_name", "None")
-        time_stamp = log.get("event_timestamp", "None")
-        ip_addr = log.get("ipaddr", "None")
+        caller = log.caller
+        event = log.properties.get('message', 'None')
+        operation_name = log.operation_name
+        time_stamp = log.event_timestamp
+        ip_addr = log.claims.get('ipaddr', 'None')
         print(caller, event, operation_name, time_stamp, ip_addr)
         exit()
     
-return_activity_logs(MYRESOURCEGROUP)
+return_activity_logs("MYRESOURCEGROUP")
