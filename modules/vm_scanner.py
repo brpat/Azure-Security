c6d6b1d50a9da0c105385368b6a7bef964ce725a
Brijesh <brpatel0807@gmail.com>
2024-10-26 17:08:58 -0400

    Updated dependencies

--- modules/vm_scanner.py
+++ modules/vm_scanner.py
@@ -2,7 +2,6 @@ from os import environ
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.compute import ComputeManagementClient
 
-
 '''
 https://learn.microsoft.com/en-us/python/api/azure-mgmt-compute/azure.mgmt.compute.computemanagementclient?view=azure-python#azure-mgmt-compute-computemanagementclient-virtual-machines
 '''
@@ -12,11 +11,12 @@ https://learn.microsoft.com/en-us/python/api/azure-mgmt-compute/azure.mgmt.compu
 
 # Class VirtualMachine ScaleSet Scanner
 
-class VirtualMachineScanner(ComputeManagementClient):
+class VirtualMachineScanner:
     def __init__(self, subscription_id:str = None):
         self.subscription_id = subscription_id
+        self.cmc_client = self.setup_compute_client(subscription_id)
 
-    def setup_compute_client(self) -> ComputeManagementClient:
+    def setup_compute_client(self, subscription_id:str) -> ComputeManagementClient:
         """Sets up the Authorization Management Client using DefaultAzureCredential."""
         try:
             cmc_client = ComputeManagementClient(
@@ -27,11 +27,15 @@ class VirtualMachineScanner(ComputeManagementClient):
         except Exception as e:
             print(f"Failed to create AuthorizationManagementClient: {e}")
             return None
+    
+    def list_all_vms(self):
+        return self.cmc_client.virtual_machines.list_all()
 
 
-class VirtualMachineScaleSetScanner:
+class VirtualMachineScaleSetScanner(ComputeManagementClient):
     pass
 
 
-
 vm_client = VirtualMachineScanner(subscription_id=environ['AZURE_SUBSCRIPTION_ID'])
+for vm in vm_client.list_all_vms():
+    print(vm)
